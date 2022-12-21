aef6c8598a65d75d2d9990ec932d621cee21ca8c
Xname <xnameserver@o2.pl>
2022-12-21 12:58:11 +0100

    Dodaj pliki projektów.

---
+++ RemoveSCP-096/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+
+namespace Mistaken.RemoveSCP_096
+{
+    internal sealed class Plugin
+    {
+        [PluginPriority(LoadPriority.Medium)]
+        [PluginEntryPoint("Remove SCP-096", "1.0.0", "Remove SCP-096 from spawn queue", "Xname")]
+        private void Load()
+        {
+            _harmony.PatchAll();
+        }
+
+        [PluginUnload]
+        private void Unload()
+        {
+            _harmony.UnpatchAll();
+        }
+
+        private static readonly Harmony _harmony = new("mistaken.rscp096.patch");
+    }
+}
---
+++ RemoveSCP-096/ScpSpawnerSpawnableScpsPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using PlayerRoles;
+using PlayerRoles.RoleAssign;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Mistaken.RemoveSCP_096
+{
+    [HarmonyPatch(typeof(ScpSpawner), nameof(ScpSpawner.SpawnableScps), MethodType.Getter)]
+    internal static class ScpSpawnerSpawnableScpsPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = NorthwoodLib.Pools.ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int index = newInstructions.FindLastIndex(x => x.opcode == OpCodes.Brfalse_S);
+            var label = generator.DefineLabel();
+            newInstructions[index + 5].WithLabels(label); // Ldloca_S V_1
+
+            newInstructions.InsertRange(index + 1, new CodeInstruction[]
+            {
+                new(OpCodes.Ldloca_S, 2),
+                new(OpCodes.Call, AccessTools.Method(typeof(KeyValuePair<RoleTypeId, PlayerRoleBase>), "get_Key")),
+                new(OpCodes.Conv_I4),
+                new(OpCodes.Ldc_I4, 9), // RoleTypeId.Scp096
+                new(OpCodes.Beq_S, label),
+            });
+
+            foreach (var instruction in newInstructions)
+                yield return instruction;
+
+            NorthwoodLib.Pools.ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
